f1ab5623ecc4d0842fe8d100536b1367b2c5f50d
Apples <2352020+apples@users.noreply.github.com>
2026-04-17 23:25:02 -0500

    Basic grid functionality

---
+++ scenes/main_gameplay/battle_grid.gd
@@ -0,0 +1,37 @@
+extends Node2D
+class_name BattleGrid
+
+signal cell_clicked(grid_pos: Vector2i)
+
+const GRID_DIM = Vector2i(20, 15)
+const CELL_SIZE = Vector2(32, 32)
+
+var _grid_bodies: Array[GridBody]
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			cell_clicked.emit(Vector2i(event.global_position / CELL_SIZE))
+
+func _draw() -> void:
+	for x in GRID_DIM.x:
+		for y in GRID_DIM.y:
+			var p = Vector2(x, y)
+			draw_rect(Rect2(p * CELL_SIZE, CELL_SIZE), Color.RED, false, 2.0)
+
+func add_body(body: GridBody) -> void:
+	assert(body not in _grid_bodies)
+	_grid_bodies.append(body)
+
+func remove_body(body: GridBody) -> void:
+	assert(body in _grid_bodies)
+	_grid_bodies.erase(body)
+
+func get_cell_center(pos: Vector2i) -> Vector2:
+	return (Vector2(pos) + Vector2(0.5, 0.5)) * CELL_SIZE
+
+func get_occupant(pos: Vector2i) -> GridBody:
+	for b in _grid_bodies:
+		if b.grid_position == pos:
+			return b
+	return null
--- scenes/main_gameplay/main_gameplay.gd
+++ scenes/main_gameplay/main_gameplay.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
-@onready var camera_shake: CameraShake = $Camera2D/CameraShake
+@onready var actor: GridBody = $BattleGrid/actor
 
-
-func _on_bouncing_character_body_2d_bounce(collision: KinematicCollision2D) -> void:
-	#camera_shake.apply_impulse(Vector2.from_angle(randf_range(0, TAU)) * 2000)
-	camera_shake.rumble(50, 0.25)
+func _on_battle_grid_cell_clicked(grid_pos: Vector2i) -> void:
+	actor.grid_position = grid_pos
