75bf9bb3144a258eb7395f1d78e805559b0d8a50
gizmolo <jacob.wallace@utexas.edu>
2026-04-18 15:17:01 -0500

    clean up parallax background script

--- objects/parallax_background/parallax_background.gd
+++ objects/parallax_background/parallax_background.gd
@@ -16,7 +16,7 @@ var segment_transition_queue = []
 func _ready():
 	var middle_straight = terrain_segments["MiddleStraight"].instantiate() #middle_straight_scene.instantiate()
 	self.add_child(middle_straight)
-	middle_to_right_segment()
+	queue_middle_to_right_segment_transition()
 
 func _process(_delta):
 	prev_scroll_offset_y = current_scroll_offset_y
@@ -25,35 +25,34 @@ func _process(_delta):
 	if current_scroll_offset_y < prev_scroll_offset_y:
 		segment_reset.emit()
 
-func middle_straight_segment():
-	segment_transition_queue.append("MiddleStraight")
-	
-func middle_to_right_segment():
+func queue_middle_to_right_segment_transition():
 	segment_transition_queue.append_array(["MiddleToRightTurn1", "MiddleToRightTurn2", "MiddleToRightTurn3", "RightStraight"])
 	
-func middle_right_segment():
-	segment_transition_queue.append("RightStraight")
+#func middle_straight_segment():
+	#segment_transition_queue.append("MiddleStraight")
+	
+#func middle_right_segment():
+	#segment_transition_queue.append("RightStraight")
 
 func _on_segment_reset():
 	if self.get_children().size() > 0:
 		for n in self.get_children():
 			if n.position.y < 0:
+				# Reset position offset to normalize segment repeat behavior.
 				n.position.y = 0
 				free_other_segments(n)
 		
-		if segment_transition_queue.size() > 0:
-			var new_terrain_segment = terrain_segments[segment_transition_queue[0]].instantiate()
-			print(new_terrain_segment.position.y)
-			new_terrain_segment.position.y -= 480
-			self.add_child(new_terrain_segment)
-			segment_transition_queue.pop_front()
+		initiate_segment_transition()
 
-func free_other_segments(n):
+func initiate_segment_transition():
+	if segment_transition_queue.size() > 0:
+		var new_terrain_segment = terrain_segments[segment_transition_queue[0]].instantiate()
+		# Set position offset to "queue" next segment above current segment.
+		new_terrain_segment.position.y -= 480
+		self.add_child(new_terrain_segment)
+		segment_transition_queue.pop_front()
+
+func free_other_segments(child_to_not_free):
 	for child in self.get_children():
-		if n != child:
+		if child_to_not_free != child:
 			child.queue_free()
-
-func free_current_segment():
-	for n in self.get_children():
-		#self.remove_child(n)
-		n.queue_free()
