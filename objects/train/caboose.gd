33e6e77bc280a0acd03006d18bb5abfaad0d5e39
gizmolo <jacob.wallace@utexas.edu>
2026-04-18 22:12:38 -0500

    caboose script organization and shimmy logic

--- objects/train/caboose.gd
+++ objects/train/caboose.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
+@onready var caboose_sprite = %Sprite2D
+
 const X_SPEED = 100
 var new_pos = null
-var moving_direction = "NONE" # | "RIGHT" | "LEFT" | "DOWN" | "UP"
+var moving_direction = "NONE" # | "RIGHT" | "LEFT" | "DOWN" ? | "UP" ?
+
+var shimmy_duration: float = 0.25
+var shimmy_intensity: float = 0.25
+var _shimmy_timer: float = 0.0
+var original_sprite_pos: Vector2
 
 func _process(delta):
+	process_shimmy(delta)
+	process_follow_movement(delta)
+
+func process_follow_movement(delta):
 	if new_pos != null:
 		if moving_direction == "RIGHT":
 			if new_pos.x > position.x:
@@ -22,6 +33,17 @@ func reset_angle():
 		moving_direction = "NONE"
 		rotation_degrees = 0
 
+func process_shimmy(delta):
+	if _shimmy_timer > 0.0:
+		_shimmy_timer -= delta
+		var offset = Vector2(
+			randf_range(-shimmy_intensity, shimmy_intensity),
+			randf_range(-shimmy_intensity, shimmy_intensity)
+		)
+		caboose_sprite.position += offset
+	else:
+		caboose_sprite.position = original_sprite_pos
+
 func _on_engine_update_train_position(target_pos: Vector2):
 	await get_tree().create_timer(0.1).timeout
 	
@@ -33,3 +55,9 @@ func _on_engine_update_train_position(target_pos: Vector2):
 		rotation_degrees -= 10
 	
 	new_pos = target_pos
+
+func initiate_shimmy():
+	_shimmy_timer = shimmy_duration
+
+func _on_shimmy_timer_timeout():
+	initiate_shimmy()
