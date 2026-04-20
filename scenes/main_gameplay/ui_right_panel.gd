ef3fd75ff50a23b64ec9c74f0733e656ad2cd07b
Apples <2352020+apples@users.noreply.github.com>
2026-04-20 08:15:13 -0500

    Fixed command menu

--- scenes/main_gameplay/ui_right_panel.gd
+++ scenes/main_gameplay/ui_right_panel.gd
@@ -1,3 +1,10 @@
 extends Panel
 
+signal go_button_pressed()
+
 @export var main_gameplay: MainGameplay
+
+@onready var turn_button: Button = %TurnButton
+
+func _on_turn_button_pressed() -> void:
+	go_button_pressed.emit()
