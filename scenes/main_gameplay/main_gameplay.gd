a55be64bf521835f72b412fb36efd318e8c7d8e3
gizmolo <jacob.wallace@utexas.edu>
2026-04-20 20:27:48 -0500

    returning mountain stay idk what happened in some merge to remove it

--- scenes/main_gameplay/main_gameplay.gd
+++ scenes/main_gameplay/main_gameplay.gd
@@ -56,6 +56,7 @@ var player_signal_points: int:
 var _selected_actor: EntityBody
 var _box_scene = preload("res://objects/ui/indicator.tscn")
 var _warning_scene = preload("res://objects/grid_terrain/warning.tscn")
+var _warning_short_scene = preload("res://objects/grid_terrain/warningShort.tscn")
 
 var turn_counter = 0
 var turn_goal = 20
@@ -352,7 +353,7 @@ func queue_mountain_smoke_left():
 			tiles.append(Vector2i(r, c))
 			
 	for i in tiles:
-		var new_warning_tile = _warning_scene.instantiate()
+		var new_warning_tile = _warning_short_scene.instantiate()
 		new_warning_tile.grid_position = i
 		battle_grid.add_child(new_warning_tile)
 
@@ -364,7 +365,7 @@ func queue_mountain_smoke_right():
 			tiles.append(Vector2i(r + starting_row, c))
 			
 	for i in tiles:
-		var new_warning_tile = _warning_scene.instantiate()
+		var new_warning_tile = _warning_short_scene.instantiate()
 		new_warning_tile.grid_position = i
 		battle_grid.add_child(new_warning_tile)
 
@@ -405,6 +406,11 @@ func _on_turn_end():
 	print("turn: %s" % turn_counter)
 	print("wave: %s" % current_wave)
 	print("level: %s" % Globals.level)
+	
+	if current_terrain_segment_state == Globals.TerrainSegmentStates.LEFT and incoming_segment != Globals.TerrainSegmentStates.MIDDLE and incoming_segment == Globals.TerrainSegmentStates.NONE:
+		queue_mountain_smoke_right()
+	if current_terrain_segment_state == Globals.TerrainSegmentStates.RIGHT  and incoming_segment != Globals.TerrainSegmentStates.MIDDLE and incoming_segment == Globals.TerrainSegmentStates.NONE:
+		queue_mountain_smoke_left()
 
 func _spawn_turn_stuff():
 	if Globals.level > 0:
