e1ec8d2cfae14a7ea0357602c6169a5dfc54b534
Josiah Harvey <jthfactor@gmail.com>
2026-04-26 12:38:15 -0500

    Small ui fixes that were bugging me

--- scenes/main_gameplay/ui_right_panel.gd
+++ scenes/main_gameplay/ui_right_panel.gd
@@ -14,21 +14,19 @@ var health_bar_max_size: float
 func _ready() -> void:
 	health_bar_max_size = engine_health_bar.size.y
 	
-	Globals.engine_health_changed.connect(set_engine_hp_bar)
-	Globals.flatbead_health_changed.connect(set_flatbead_hp_bar)
-	Globals.caboose_health_changed.connect(set_caboose_hp_bar)
+	Globals.engine_health_changed.connect(func(progress: float): set_train_hp_bar(engine_health_bar, progress))
+	Globals.flatbead_health_changed.connect(func(progress: float): set_train_hp_bar(flatbead_health_bar, progress))
+	Globals.caboose_health_changed.connect(func(progress: float): set_train_hp_bar(caboose_health_bar, progress))
 
 func _on_turn_button_pressed() -> void:
 	go_button_pressed.emit()
 
-func set_engine_hp_bar(progress: float) -> void:
-	engine_health_bar.size = Vector2(engine_health_bar.size.x, health_bar_max_size * progress)
-	engine_health_bar.position = Vector2(1, 1 + health_bar_max_size * (1 - progress))
-
-func set_flatbead_hp_bar(progress: float) -> void:
-	flatbead_health_bar.size = Vector2(flatbead_health_bar.size.x, health_bar_max_size * progress)
-	flatbead_health_bar.position = Vector2(1, 1 + health_bar_max_size * (1 - progress))
-
-func set_caboose_hp_bar(progress: float) -> void:
-	caboose_health_bar.size = Vector2(caboose_health_bar.size.x, health_bar_max_size * progress)
-	caboose_health_bar.position = Vector2(1, 1 + health_bar_max_size * (1 - progress))
+func set_train_hp_bar(bar: NinePatchRect, progress: float) -> void:
+	var new_bar_size_y: float = health_bar_max_size * progress
+	if new_bar_size_y < 4:
+		bar.hide()
+		return
+	
+	bar.show()
+	bar.size = Vector2(bar.size.x, health_bar_max_size * progress)
+	bar.position = Vector2(1, 1 + health_bar_max_size * (1 - progress))
